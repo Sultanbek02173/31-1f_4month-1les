2c1b120cd2c4b3e42b3a458f289d1495a5725807
Sultanbek02173 <sultan.02173@gmail.com>
2025-03-31 20:23:00 +0600

    first init

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,13 @@
+import './banner.scss';
+import banner from '../../imgs/IMAGE(1).png'
+
+const Banner = () => {
+    return (
+        <div>
+            <h1>banner</h1>
+            <img src={banner} alt="" />
+        </div>
+    );
+}
+
+export default Banner;
