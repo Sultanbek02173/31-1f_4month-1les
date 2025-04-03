5d070911743d064c24f04dea3c640f1b283093d1
Sultanbek02173 <sultan.02173@gmail.com>
2025-04-03 18:47:03 +0600

    create banner

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,11 +1,19 @@
 import './banner.scss';
-import banner from '../../imgs/IMAGE(1).png'
+import banner from '../../imgs/IMAGE(1).png';
+import social from '../../imgs/Frame.png';
 
 const Banner = () => {
     return (
-        <div>
-            <h1>banner</h1>
-            <img src={banner} alt="" />
+        <div className='banner'>
+            <div className='banner-image'>
+                <img src={banner} alt="" /> 
+            </div>
+            <div className='banner-text'>
+                <h1 className='banner-text-title'>
+                    Take your podcast to the next <span>level</span>
+                </h1>
+                <img src={social} alt="" />
+            </div>
         </div>
     );
 }
